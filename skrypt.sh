d9739383086b9ec19136f02d22e2fa8d6db65e94
Paulina <paulinka6105@onet.pl>
2022-07-15 18:09:38 +0200

    dodanie logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 case "$1" in
-	--data|-d) echo "dzisiejsza data to: $(date)";;
-	
+	--data) echo "dzisiejsza data to: $(date)";;
+	--logs) for (( c=1; c<=100; c++))
+					do 
+						echo "log$c.txt, $0, $(date)" > log$c.txt
+					done
+			
+			;;
 esac
